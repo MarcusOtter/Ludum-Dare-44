e0d166b4cfdec4d86b87d2a130af9399645dd8b6
Marcus Otterström <marcus.otterstrom@gmail.com>
2019-04-29 22:57:15 +0200

    Start adding inventory

---
+++ Ludum dare 44/Assets/Scripts/Inventory.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Inventory")]
+public class Inventory : ScriptableObject
+{
+    [SerializeField] private List<GameObject> gameObjects;
+}
---
+++ Ludum dare 44/Assets/Scripts/InventoryManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    internal static InventoryManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        SingletonSetup();
+    }
+
+    
+
+    private void SingletonSetup()
+    {
+        if (Instance is null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
