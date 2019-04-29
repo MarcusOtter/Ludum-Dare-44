9b6a2877e74c801bb483b52b33de44c24fb7300b
Marcus Otterström <marcus.otterstrom@gmail.com>
2019-04-29 22:11:14 +0200

    Add soul animation

--- Ludum dare 44/Assets/Scripts/VictimGraphics.cs
+++ Ludum dare 44/Assets/Scripts/VictimGraphics.cs
@@ -4,11 +4,13 @@
 public class VictimGraphics : MonoBehaviour
 {
     [SerializeField] private string _soulTransformationName;
+    [SerializeField] private string _getConsumedName = "GetConsumed";
 
     private Animator _animator;
     private Rigidbody2D _rigidbody;
 
     private int _soulTransformationHash;
+    private int _getConsumedHash;
 
     private void Awake()
     {
@@ -16,11 +18,13 @@ public class VictimGraphics : MonoBehaviour
         _rigidbody = GetComponent<Rigidbody2D>();
 
         _soulTransformationHash = Animator.StringToHash(_soulTransformationName);
+        _getConsumedHash = Animator.StringToHash(_getConsumedName);
     }
 
     private void OnEnable()
     {
         Victim.OnDeath += TransformToSoul;
+        Victim.OnConsumed += GetConsumed;
     }
 
     private void TransformToSoul(object sender, System.EventArgs args)
@@ -28,9 +32,14 @@ public class VictimGraphics : MonoBehaviour
         _animator.SetTrigger(_soulTransformationHash);
     }
 
+    private void GetConsumed(object sender, System.EventArgs args)
+    {
+        _animator.SetTrigger(_getConsumedHash);
+    }
+
     private void OnDisable()
     {
         Victim.OnDeath -= TransformToSoul;
-
+        Victim.OnConsumed -= GetConsumed;
     }
 }
